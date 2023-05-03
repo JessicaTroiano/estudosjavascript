b92f3c73799d56a5aa4abe518134db454a355f85
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-05-02 23:10:42 -0300

    feat: add arquivo numeros

---
+++ fundamentos/numeros.js
@@ -0,0 +1,20 @@
+const peso1 = 1.0;
+
+// Number('1'), transforma strings em número
+const peso2 = Number('1.2');
+
+console.log(peso1, peso2);
+console.log(Number.isInteger(peso1))
+// Number.isInteger() verifica se o número é um inteiro.
+console.log(Number.isInteger(peso2))
+
+const avaliacao1 = 9.871
+const avaliacao2 = 6.871
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2
+const media = total / (peso1 + peso2);
+
+//toFixed(), fixa a quantidade de números após a virgula
+console.log(media.toFixed(2));
+//toString(), transforma um número em string;
+console.log(media.toString());
