dea6c6c738a3d06e8e95d0772385c1f98df29040
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-05-02 23:01:43 -0300

    feat: inclui arquivo tipagemFraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,8 @@
+
+// JavaScript é uma linguagem fracamente tipada, diferente de linguagens fortemente tipadas como Java ou C#
+let qualquer = 'Legal';
+
+console.log(qualquer);
+
+// typeof verifica o tipo da variável;
+console.log(typeof qualquer);
