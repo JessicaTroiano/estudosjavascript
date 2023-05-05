43db39ca2a666a67361cc9c90d2badbac3f45a20
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-05-04 23:09:25 -0300

    feat: add arquivo 1003

---
+++ exercicios/1003.js
@@ -0,0 +1,12 @@
+// var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+// var lines = input.split('\n');
+
+// const A = parseInt(lines[0])
+// const B = parseInt(lines[1])
+
+const A = parseInt(30)
+const B = parseInt(10)
+
+let soma = A + B;
+
+console.log("SOMA = " + soma)
