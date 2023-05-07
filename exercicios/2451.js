9300e5771ee744d540a8848713797a8f9ea09ac7
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-05-06 23:58:57 -0300

    feat: altera desafio 2451

--- exercicios/2451.js
+++ exercicios/2451.js
@@ -1,16 +1,26 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
+// var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+// var lines = input.split('\n');
 
-let N = Number(lines[0]);
+let N = 5;
 
 let matriz = new Array(N);
-matriz[N] = new Array(N);
 
 let vazio = ""
 let comida = "o"
 let fantasma = "A"
 
-let posicao = matriz[0][0]
-if(!posicao){
-    
+for (let i = 0; i < N; i++) {
+    matriz[i] = new Array(N);
+    if(i === 0){
+        matriz[i].splice(0, 1, vazio);
+    }else{
+        matriz[i].splice(0, 1, fantasma);
+    }
+    matriz[i].splice(1, 1, comida);
+    matriz[i].splice(2, 1, comida);
+    matriz[i].splice(3, 1, fantasma);
+    matriz[i].splice(4, 1, vazio);
+    console.log(i)
 }
+
+(console.log(matriz))
