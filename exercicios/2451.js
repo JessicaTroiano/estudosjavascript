07da7d1808ee8feeb3e27615116cea35c235b2a3
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-05-09 00:25:31 -0300

    feat: add contador de elementos

--- exercicios/2451.js
+++ exercicios/2451.js
@@ -14,18 +14,12 @@ const opcoes = {
 const vazioInicial = '.';
 
 const keys = Object.keys(opcoes);
-console.log(keys.length)
+// console.log(keys.length)
 
-
-
-for (let i = 0; i < N; i++) {
-  matriz[i] = new Array(N);
-  if (i === 0) {
-    matriz[i].splice(0, 1,  vazioInicial);
-  } else {
-    null;
-  }
-
+const sorteio = (key) => {
+  const prob1 = 0.5; // 50% de chance de sorteio
+  const prob2 = 0.25; // 25% de chance de sorteio
+  const prob3 = 0.25; // 25% de chance de sorteio
   for(let i = keys.length -1; i > 0; i--){
     //Math.floor()
     //A função Math.floor(x) retorna o menor número inteiro dentre o número "x".
@@ -33,19 +27,52 @@ for (let i = 0; i < N; i++) {
     //Math.random()
     //A função Math.random() retorna um número pseudo-aleatório no intervalo [0, 1], ou seja, de 0 (inclusivo) até, mas não incluindo, 1 (exclusivo).
     const j = Math.floor(Math.random() * (i+1));
-    // console.log('j', j);
-    // console.log('i', i);
-    // console.log([keys[i], keys[j]])
-    [keys[i], keys[j]] = [keys[j], keys[i]]
-  }
-
-  for(const key of keys){
-    console.log(opcoes[key]);
-    matriz[i].splice(1, 1, opcoes[key]);
-    matriz[i].splice(2, 1,  opcoes[key]);
-    matriz[i].splice(3, 1,  opcoes[key]);
-    matriz[i].splice(4, 1,  opcoes[key]);
+    const troca = [keys[i], keys[j]] = [keys[j], keys[i]]
+    for(key of keys){
+      return opcoes[key];
+    }
   }
 }
 
-(console.log(matriz))
+function countItems(matriz) {
+  const countMap = Object.create(null);
+
+  for (const element of matriz) {
+    if (!countMap[element]) {
+      countMap[element] = 1;
+    } else {
+      countMap[element] += 1;
+    }
+  }
+  
+  return countMap;
+}
+
+for (let i = 0; i < N; i++) {
+  matriz[i] = new Array(N);
+
+    matriz[i].splice(0, 1, sorteio(i));
+    matriz[i].splice(1, 1, sorteio(i));
+    matriz[i].splice(2, 1, sorteio(i));
+    matriz[i].splice(3, 1,  sorteio(i));
+    matriz[i].splice(4, 1,  sorteio(i));
+
+    if (i === 0) {
+      matriz[i].splice(0, 1,  vazioInicial);
+    } else {
+      null;
+    }
+
+    console.log(countItems(matriz[i]))
+
+    // const contar = 0;
+    // if(matriz[i] === opcoes.comida){
+    //   const contagem = contar++
+    //   console.log(contagem)
+    //   return contagem;
+    // }
+    // console.log(contar);
+}
+
+
+(console.log(matriz)) 
