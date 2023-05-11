57f4d6c7a3087cf0d12856c4d7dceda6fe69a088
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-05-10 23:50:27 -0300

    feat: altera desafio 2451

--- exercicios/2451.js
+++ exercicios/2451.js
@@ -17,9 +17,9 @@ const keys = Object.keys(opcoes);
 // console.log(keys.length)
 
 const sorteio = (key) => {
-  const prob1 = 0.5; // 50% de chance de sorteio
-  const prob2 = 0.25; // 25% de chance de sorteio
-  const prob3 = 0.25; // 25% de chance de sorteio
+  // const prob1 = 0.5; // 50% de chance de sorteio
+  // const prob2 = 0.25; // 25% de chance de sorteio
+  // const prob3 = 0.25; // 25% de chance de sorteio
   for(let i = keys.length -1; i > 0; i--){
     //Math.floor()
     //A função Math.floor(x) retorna o menor número inteiro dentre o número "x".
@@ -27,31 +27,17 @@ const sorteio = (key) => {
     //Math.random()
     //A função Math.random() retorna um número pseudo-aleatório no intervalo [0, 1], ou seja, de 0 (inclusivo) até, mas não incluindo, 1 (exclusivo).
     const j = Math.floor(Math.random() * (i+1));
-    const troca = [keys[i], keys[j]] = [keys[j], keys[i]]
+    [keys[i], keys[j]] = [keys[j], keys[i]]
     for(key of keys){
       return opcoes[key];
     }
   }
 }
 
-function countItems(matriz) {
-  const countMap = Object.create(null);
-
-  for (const element of matriz) {
-    if (!countMap[element]) {
-      countMap[element] = 1;
-    } else {
-      countMap[element] += 1;
-    }
-  }
-  
-  return countMap;
-}
-
 for (let i = 0; i < N; i++) {
   matriz[i] = new Array(N);
 
-    matriz[i].splice(0, 1, sorteio(i));
+    matriz[i].splice(0, 1, sorteio(i)); //substitui item em determinada posição de um array. O primeiro valor corresponde ao indice no array, e o segundo a quantidade de itens que devem ser substituidos.
     matriz[i].splice(1, 1, sorteio(i));
     matriz[i].splice(2, 1, sorteio(i));
     matriz[i].splice(3, 1,  sorteio(i));
@@ -62,17 +48,18 @@ for (let i = 0; i < N; i++) {
     } else {
       null;
     }
-
-    console.log(countItems(matriz[i]))
-
-    // const contar = 0;
-    // if(matriz[i] === opcoes.comida){
-    //   const contagem = contar++
-    //   console.log(contagem)
-    //   return contagem;
-    // }
-    // console.log(contar);
 }
 
+console.log(matriz)
 
-(console.log(matriz)) 
+let contador = 0;
+for (let i = 0; i < matriz.length; i++) {
+  for (let j = 0; j < matriz[i].length; j++) {
+    if (matriz[i][j] === "o") {
+      contador++;
+      console.log(contador)
+    }
+  }
+}
+
+const contar = matriz.filter((item) => item.includes(opcoes.comida)); // cria um novo array/matriz que constem o valor passado para o método includes
